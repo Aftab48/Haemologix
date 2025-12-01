4ee15c259f0f602a9342bbc9febff97f83570f71
Aftab Alam <mdalam4884@gmail.com>
2025-12-01 23:35:53 +0530

    extraction admin and donor page contents into components

---
+++ app/admin/AdminMobile.tsx
@@ -0,0 +1,12 @@
+import GradientBackground from '@/components/GradientBackground';
+import React from 'react'
+
+function AdminMobileDashboard() {
+  return (
+    <GradientBackground className="flex flex-col">
+        <div>AdminMobile</div>
+    </GradientBackground>
+  )
+}
+
+export default AdminMobileDashboard;
---
+++ app/donor/DonorMobile.tsx
@@ -0,0 +1,10 @@
+import GradientBackground from '@/components/GradientBackground';
+import React from 'react';
+
+function DonorMobileDashboard() {
+  return <GradientBackground className="flex flex-col">
+    <div>DonorMobile</div>
+  </GradientBackground>;
+}
+
+export default DonorMobileDashboard;
